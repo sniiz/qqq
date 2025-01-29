7a74a84f0b0faa9534fdf27b2e4782489a1cc818
sniiz <haley@program.mr>
2025-01-29 12:36:09 +0300

    bigger data

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,3 @@
 import pandas as pd
 
-# i feel like the model is being messed up by enormous scores for checkmates
 data = pd.read_csv("bigdata.csv")
-data = data[data["score"] <= 90000]
-data.to_csv("bigdata.csv", index=False)
